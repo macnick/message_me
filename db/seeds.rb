95a5e40cbe3b7fb7229c1ac9870b6b4f2f08a5dc
macnick <nick@macexperts.gr>
2019-12-20 17:16:09 +0200

    Build user resourse, add hirb gem for nice viewing in rails console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Nick Haras", password: "123456")
+User.create(username: "Daniel Addo", password: "123456")
+User.create(username: "Clint Eastwood", password: "123456")
+User.create(username: "Robert De Niro", password: "123456")
+User.create(username: "Hugh Jackman", password: "123456")
