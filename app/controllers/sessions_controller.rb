4f84c8ad92754d154008f01e31ff9edb2192f7c7
macnick <nick@macexperts.gr>
2019-12-23 17:44:01 +0200

    Restrict views at controller layer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
-
-  def new
+  before_action :logged_in_redirect, only: [:new, :create]
   
+  def new
   end
 
   def create
@@ -22,4 +22,11 @@ class SessionsController < ApplicationController
     redirect_to login_path
   end
 
+  private
+    def logged_in_redirect
+      if logged_in?
+        flash[:error] = "You are already logged in"
+        redirect_to root_path
+      end
+    end
 end
