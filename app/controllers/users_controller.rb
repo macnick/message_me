b32d587d2a9a32027973727f8130efafd3f96a9e
macnick <nick@macexperts.gr>
2019-12-27 11:31:12 +0200

    Fix signup with auto login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # autologin
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the chatroom #{@user.username}"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
   get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
 
   mount ActionCable.server, at: '/cable'
 end
